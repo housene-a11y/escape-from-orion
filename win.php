bd778ade3b666568648d7d0b5ad00de219fca1c5
moo107 <9030671@student.zadkine.nl>
2026-05-27 21:44:21 +0200

    Style.css
    .

---
+++ win.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ECLIPS‑7 – Missie Geslaagd</title>
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+
+  <div class="stars"></div>
+  <div class="stars2"></div>
+  <div class="stars3"></div>
+
+  <h1>Missie Geslaagd!</h1>
+
+  <p>
+    Je hebt de noodcapsule weten te openen en bent ontsnapt voordat de
+    <strong>ECLIPS‑7</strong> ontplofte.
+  </p>
+
+  <p>
+    Dankzij jouw snelle denken en teamwork is de missie succesvol afgerond.
+  </p>
+
+  <h2>Je hebt gewonnen!</h2>
+
+  <button>
+    <a href="index.php">Terug naar de homepage</a>
+  </button>
+
+</body>
+
+</html>
