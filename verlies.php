06350e3baa34786391c0908bbd0607139c0661d3
moo107 <9030671@student.zadkine.nl>
2026-05-27 21:55:46 +0200

    verlies-pagina
    .

---
+++ verlies.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ECLIPS‑7 – Missie Mislukt</title>
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+
+  <div class="stars"></div>
+  <div class="stars2"></div>
+  <div class="stars3"></div>
+
+  <h1 class="alert">MISSIE MISLUKT</h1>
+
+  <p class="shake">
+    De <strong>ECLIPS‑7</strong> is ontploft voordat je de noodcapsule kon openen.
+  </p>
+
+  <p>
+    De systemen vielen uit, de tijd was op… en de missie is verloren.
+  </p>
+
+  <h2>Je hebt verloren.</h2>
+
+  <button class="retry-btn">
+    <a href="index.php">Probeer opnieuw</a>
+  </button>
+
+</body>
+
+</html>
